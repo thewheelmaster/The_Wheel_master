88ee8b8a6608c4ce0635e57434c92d5c6b393ec7
Vatsal Bhatt <bhattvatsal534@gmail.com>
2025-08-26 10:10:15 +0530

    mnv

--- src/components/Sliders/HeroSlider.jsx
+++ src/components/Sliders/HeroSlider.jsx
@@ -182,19 +182,6 @@ const HeroSlider = () => {
       <div className="hero-pagination">
         <div className="hero-swiper-pagination"></div>
       </div>
-      <div className="social-hero">
-        <Link to="https://www.x.com/" className="social-icon1">
-          <img src="/assets/img/icon/twiter.svg" alt="twitericon" />
-        </Link>
-        <Link to="https://www.facebook.com/" className="social-icon1">
-          <img src="/assets/img/icon/facebook.svg" alt="twitericon" />
-        </Link>
-        <Link to="https://www.linkedin.com/" className="social-icon1">
-          <img src="/assets/img/icon/linkedin.svg" alt="twitericon" />
-        </Link>
-        <div className="social-horizontal"></div>
-        <h6 className="social-link">FOLLOW US</h6>
-      </div>
     </section>
   );
 };
